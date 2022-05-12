bf0f938370ea03e2685f2d30e30e120f63faed20
Geovani Paz <geovanipaz7@gmail.com>
2022-05-12 10:34:45 -0300

    signup

--- My_Ecom_Project/App_Login/views.py
+++ My_Ecom_Project/App_Login/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.http import HttpResponse
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+
+from App_Login.models import Profile
+from App_Login.forms import ProfileForm, SignUpForm
 # Create your views here.
+
+
+def sign_up(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse)
+    return render(request, 'App_Login/sign_up.html', context={'form':form})
