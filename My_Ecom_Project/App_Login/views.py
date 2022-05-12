657d01fb599d9e19a8e9e942b56d448b3eb76eaf
Geovani Paz <geovanipaz7@gmail.com>
2022-05-12 14:14:23 -0300

    login

--- My_Ecom_Project/App_Login/views.py
+++ My_Ecom_Project/App_Login/views.py
@@ -17,5 +17,23 @@ def sign_up(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse)
+            return HttpResponseRedirect(reverse('App_Login:login'))
     return render(request, 'App_Login/sign_up.html', context={'form':form})
+
+def login_user(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user =  authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponse('Logged in')
+    return render(request, 'App_Login/login.html', context={'form':form})
+
+@login_required        
+def logout_user(request):
+    logout()
+    return HttpResponse('Logged out')
