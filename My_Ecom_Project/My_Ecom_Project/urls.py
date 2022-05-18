7fc22e8c653a33d5d98ca342efb0fece1b6f6015
Geovani Paz <geovanipaz7@gmail.com>
2022-05-17 23:19:59 -0300

    imple. de pedido

--- My_Ecom_Project/My_Ecom_Project/urls.py
+++ My_Ecom_Project/My_Ecom_Project/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('App_Shop.urls')),
     path('accounts/',include('App_Login.urls')),
+    path('shop/',include('App_Order.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
